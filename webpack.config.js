1384823816c36f13e22de06e6e0bfa7a1ef3e1c9
Stanislav Zhukovskiy <stanislavzhukovskiy@gmail.com>
2018-01-10 08:24:43 +0200

    Setup Webpack loaders configuration

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,15 @@ const config = {
             {
                 test: /\.(js|jsx)$/,
                 loader: 'babel-loader'
-            }
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader']
+            },
         ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
     }
 };
 
